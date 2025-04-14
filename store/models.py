297731e7392b8ce63ce990c52909f952dc94bb58
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-14 14:09:08 -0600

    Fixed typos

--- store/models.py
+++ store/models.py
@@ -10,12 +10,14 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.FloatField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collections = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -75,5 +77,5 @@ class CartItem(models.Model):
 class Address (models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.ForeignField(
+    customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE)
