79a1f4ba69666ffcb400e536843532430c6b94ad
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-14 14:18:13 -0600

    Created likes and tag store, with generci fields

--- likes/models.py
+++ likes/models.py
@@ -5,12 +5,8 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
