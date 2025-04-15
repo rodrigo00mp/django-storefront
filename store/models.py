5b5bf0d8d18f203b0e9b9cc6f9fe2e93cfa3a695
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-15 15:22:18 -0600

    Made db migrations

--- store/models.py
+++ store/models.py
@@ -17,6 +17,7 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    slug = models.SlugField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
