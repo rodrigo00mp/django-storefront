29d4ebe6113ecc388b12f3d2c36e369c9c0360b8
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-17 13:34:14 -0600

    Added Customer and Order models to adminsite

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'inventory_status', 'collections']
+    list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Okay'
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_per_page = 10
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
