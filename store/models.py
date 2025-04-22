a2a128c7715b253d460a1eaca87eaabf246911de
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-22 10:32:38 -0600

    Added UUID to Cart model and added restraint to cart item model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from uuid import uuid4
 # Create your models here.
 
 
@@ -86,13 +86,18 @@ class OrderItem(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 
 class Address (models.Model):
