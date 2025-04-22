02f09481d1e50960655416061ab30e00f9edc4b9
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-22 11:10:19 -0600

    Implemented getting a cart

--- store/urls.py
+++ store/urls.py
@@ -6,10 +6,12 @@ from . import views
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
-router.register('carts', views.CartViewSet)
+router.register('carts', views.CartViewSet, basename='carts')
 
 products_router = routers.NestedDefaultRouter(
     router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet,
                          basename='product-review')
+
+
 urlpatterns = router.urls + products_router.urls
