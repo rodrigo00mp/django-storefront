599b2cd9e7d1a8ec0f660b7e53f27b4d3d633b96
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-25 16:41:00 -0600

    Added first and last name to user registering

--- storefront/settings.py
+++ storefront/settings.py
@@ -141,9 +141,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
-    )
+    ]
 }
 
 SIMPLE_JWT = {
@@ -151,3 +151,9 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'core.User'
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer'
+    }
+}
