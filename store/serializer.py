2c627302f0412b2d28d935ea037003e184fdcea8
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-28 11:16:28 -0600

    Implemented data validation for Order creation

--- store/serializer.py
+++ store/serializer.py
@@ -136,6 +136,13 @@ class OrderSerializer(serializers.ModelSerializer):
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
+    def validate_cart_id(self, cart_id):
+        if Cart.objects.fiter(pk=cart_id).exists():
+            raise serializers.ValidationError('No cart with the given id')
+        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+            raise serializers.ValidationError('Cart is empty')
+        return cart_id
+
     def save(self, **kwargs):
         with transaction.atomic():
             cart_id = self.validated_data['cart_id']
