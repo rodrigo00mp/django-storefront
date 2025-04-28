3813b7e5aff7b092c3806c8b43a81ea6eab7eef0
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-28 10:39:12 -0600

    Implemented endpoint to get the orders

--- store/serializer.py
+++ store/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from decimal import Decimal
-from .models import Collection, Product, Review, Cart, CartItem, Customer
+from .models import Collection, Product, Review, Cart, CartItem, Customer, Order, OrderItem
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -112,3 +112,21 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    product = SimpleProductSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'unit_price', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'placed_at',
+                  'payment_status', 'items']
