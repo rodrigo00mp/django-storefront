a6d84811ffa8d6a2c887c26baa567116a2eed4bb
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-28 11:05:13 -0600

    Wrapped save order operations in a transaction

--- store/serializer.py
+++ store/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from decimal import Decimal
+from django.db import transaction
 from .models import Collection, Product, Review, Cart, CartItem, Customer, Order, OrderItem
 
 
@@ -136,17 +137,20 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        (customer, created) = Customer.objects.get_or_create(
-            user_id=self.context['user_id'])
-        Order.objects.create(customer=customer)
-        cart_items = CartItem.objects.select_related(
-            'product').filter(cart_id=self.validated_data['cart_id'])
-        order_items = [
-            OrderItem(
-                order=order,
-                product=item.product,
-                price=item.product.price,
-                quantity=item.quantity
-            ) for item in cart_items
-        ]
-        OrderItem.objects.bulk_create(order_items)
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            (customer, created) = Customer.objects.get_or_create(
+                user_id=self.context['user_id'])
+            Order.objects.create(customer=customer)
+            cart_items = CartItem.objects.select_related(
+                'product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    price=item.product.price,
+                    quantity=item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+            Cart.objects.filter(pk=cart_id).delete()
