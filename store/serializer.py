57813873e002704e44ed379e49fc65336aecb432
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-04-28 11:01:31 -0600

    Implemented creating order items

--- store/serializer.py
+++ store/serializer.py
@@ -136,9 +136,17 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
-
         (customer, created) = Customer.objects.get_or_create(
             user_id=self.context['user_id'])
         Order.objects.create(customer=customer)
+        cart_items = CartItem.objects.select_related(
+            'product').filter(cart_id=self.validated_data['cart_id'])
+        order_items = [
+            OrderItem(
+                order=order,
+                product=item.product,
+                price=item.product.price,
+                quantity=item.quantity
+            ) for item in cart_items
+        ]
+        OrderItem.objects.bulk_create(order_items)
