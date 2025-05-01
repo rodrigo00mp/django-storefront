c393d109de93b7e650049dbdb6e739c8e2f22e73
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-05-01 12:12:07 -0600

    Set up image validation

--- store/models.py
+++ store/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from uuid import uuid4
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, FileExtensionValidator
 from django.conf import settings
 from django.contrib import admin
+from .validators import validate_file_size
 
 # Create your models here.
 
@@ -45,7 +46,8 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/image')
+    image = models.ImageField(upload_to='store/image',
+                              validators=[validate_file_size])
 
 
 class Customer(models.Model):
