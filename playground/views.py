9eb428bedba8420998955b8794c508371222f000
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-05-01 16:13:15 -0600

    Created and executed test task

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, OrderItem, Order, Collection
-
+from .tasks import notify_customers
 # Create your views here.
 
 
 def say_hello(request):
-
+    notify_customers.delay('Hello')
     return render(request, 'hello.html', {'name': 'Rodrigo'})
