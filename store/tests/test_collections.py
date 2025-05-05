a2227987209d1fdddf84a5aa18746a9db07ddeaa
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-05-05 16:46:13 -0600

    Created test retrieve collections

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,23 +1,43 @@
-from rest_framework.test import APIClient
 from rest_framework import status
+from store.models import Collection
+from model_bakery import baker
 import pytest
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
         # Arrange
         # Act
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin(self):
+    def test_if_user_is_not_admin(self, api_client, create_collection):
         # Arrange
         # Act
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
+        api_client.force_authenticate(user={})
+        response = create_collection({'title': 'a'})
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
