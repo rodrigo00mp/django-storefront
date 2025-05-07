5c022732d877c35ee329ead4b0029da13db419e2
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-05-06 21:46:35 -0600

    Configured redis cache

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .tasks import notify_customers
+import requests
 # Create your views here.
 
 
 def say_hello(request):
-    notify_customers.delay('Hello')
+    requests.get('https://httpbin.org/delay/2')
     return render(request, 'hello.html', {'name': 'Rodrigo'})
