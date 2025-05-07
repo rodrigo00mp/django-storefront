1c47cbbfd24a0d4fd22a006917661a22858ef8b0
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-05-06 21:57:08 -0600

    Implemented caching views

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.cache import cache
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
+from django.views.decorators.cache import cache_page
 from .tasks import notify_customers
 import requests
 # Create your views here.
 
 
-def say_hello(request):
-    requests.get('https://httpbin.org/delay/2')
-    return render(request, 'hello.html', {'name': 'Rodrigo'})
+class HelloView(APIView):
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})
