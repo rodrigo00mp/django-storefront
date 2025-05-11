ba2eaac62aaeefee1bb4e7ce9efed9ea6efd730b
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-05-11 11:45:32 -0600

    Provisioned mysql database using dj-database-url

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,5 +1,6 @@
 from .common import *
 import os
+import dj_database_url
 load_dotenv(override=True)
 
 
@@ -9,3 +10,7 @@ ALLOWED_HOSTS = ['rmp-ecomm-prod-ae9dff4c41c3.herokuapp.com']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
